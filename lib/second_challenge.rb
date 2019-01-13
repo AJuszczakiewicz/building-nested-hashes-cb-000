f7d7063d08212ff47dad9bcbc37e17fbd6c9f047
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-13 14:51:17 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      patriarch: {}
-      },
+      patriarch: {},
       matriarch: {},
       hero: {},
-      hero_friends:{}}
+      hero_friends:{}
+    }
     :capulet => {
       #your key/value pairs here!
       patriarch: {},
       matriarch: {},
       heroine: {},
       heroine_friends: {}
-      }}
+    }
+  }
 
 
 end
